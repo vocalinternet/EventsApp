94c79d0222bb6debe00e87bd2441000403c16373
KayMaN777 <kayman2005@yandex.ru>
2022-07-21 13:05:35 +0300

    всё добавил, надо чинить

--- app/src/main/java/com/eventsapp/MainActivity.kt
+++ app/src/main/java/com/eventsapp/MainActivity.kt
@@ -1,27 +1,18 @@
 package com.eventsapp
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import com.eventsapp.databinding.ActivityMainBinding
-
-@SuppressLint("StaticFieldLeak")
-private var mainBinding: ActivityMainBinding? = null
-private val binding get()= mainBinding!!
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-        println("Hello world!")
-    }
-    }
+        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
+        setContentView(R.layout.activity_main)
+        //println("Hello world!")
+        val mapview = findViewById<MapView>(R.id.mapview)
+        //mapview.onCreate(savedInstanceState)
 
+    }
+}
