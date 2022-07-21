c9aa0ffeaa3a91bd761663ae9544dfbceeea7af7
KayMaN777 <kayman2005@yandex.ru>
2022-07-21 15:57:04 +0300

    добавил mapbox

--- app/src/main/java/com/eventsapp/MainActivity.kt
+++ app/src/main/java/com/eventsapp/MainActivity.kt
@@ -2,17 +2,22 @@ package com.eventsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.MapboxMap
+//import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var map: MapboxMap
+    lateinit var permissionsManager: PermissionsManager
+    lateinit var locationEngine: LocationEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
-        //println("Hello world!")
         val mapview = findViewById<MapView>(R.id.mapview)
-        //mapview.onCreate(savedInstanceState)
 
     }
 }
