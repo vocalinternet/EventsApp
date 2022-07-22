fedcaf0afc064e21a23f3c234f1614027b323762
KayMaN777 <kayman2005@yandex.ru>
2022-07-22 13:28:50 +0300

    добавил mapbox и точки

--- app/src/main/java/com/eventsapp/MainActivity.kt
+++ app/src/main/java/com/eventsapp/MainActivity.kt
@@ -1,23 +1,96 @@
 package com.eventsapp
-
-import androidx.appcompat.app.AppCompatActivity
+/*import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.mapbox.android.core.location.LocationEngine
-import com.mapbox.android.core.permissions.PermissionsManager
-import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.maps.MapView
-import com.mapbox.mapboxsdk.maps.MapboxMap
-//import kotlinx.android.synthetic.main.activity_main.*
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 
 class MainActivity : AppCompatActivity() {
-    lateinit var map: MapboxMap
-    lateinit var permissionsManager: PermissionsManager
-    lateinit var locationEngine: LocationEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Mapbox.getInstance(applicationContext, getString(R.string.mapbox_access_token))
         setContentView(R.layout.activity_main)
-        val mapview = findViewById<MapView>(R.id.mapview)
+        val map = findViewById<MapView>(R.id.mapView)
+        map.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+    }
+    //fun AddtoMap()
+}*/
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.widget.Button
+import androidx.annotation.DrawableRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import com.mapbox.geojson.Point
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+
+class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+    var cnt = 0
+    val opa:List<Pair<Double, Double>> = listOf<Pair<Double, Double>>(Pair(27.77, 17.72), Pair(14.55, 23.16), Pair(14.77, 23.54), Pair(13.55, 23.76), Pair(14.01, 23.78))
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mapView = findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(
+            Style.MAPBOX_STREETS,
+            object : Style.OnStyleLoaded {
+                override fun onStyleLoaded(style: Style) {
+                    for (i in opa) {
+                        addAnnotationToMap(i.first, i.second)
+                    }
+                }
+            }
+        )
+    }
+
+    private fun addAnnotationToMap(x : Double, y :Double) {
+// Create an instance of the Annotation API and get the PointAnnotationManager.
+        bitmapFromDrawableRes(
+            this@MainActivity,
+            R.drawable.mapbox_user_icon_shadow
+        )?.let {
+            val annotationApi = mapView?.annotations
+            val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
+// Set options for the resulting symbol layer.
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+// Define a geographic coordinate.
+                .withPoint(Point.fromLngLat(x, y))
+// Specify the bitmap you assigned to the point annotation
+// The bitmap will be added to map style automatically.
+                .withIconImage(it)
+// Add the resulting pointAnnotation to the map.
+            pointAnnotationManager?.create(pointAnnotationOptions)
+        }
+    }
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+// copying drawable object to not manipulate on the same reference
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
     }
 }
